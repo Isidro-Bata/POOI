1d58234357955bf2a435940d1808f686145f0d75
vm <vm@Unknown>
2021-08-01 00:30:21 +0200

    fornecedor

--- src/gestao/Fornecedor.java
+++ src/gestao/Fornecedor.java
@@ -1,17 +1,83 @@
 package gestao;
+
 import java.util.*;
 import java.io.*;
+
 public class Fornecedor implements Serializable{
-    private String nome, endereco, telefone, tipoFornecedor, produto;
-    private Integer quantidade, id;
-    private BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String tipoFornecedor;
+    private String produto;
+    private int quantidade;
+    private int id;
 
-    public Fornecedor(){}
+    public Fornecedor(String nome, String endereco, String telefone, String tipoFornecedor, String produto, int quantidade, int id) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.tipoFornecedor = tipoFornecedor;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.id = id;
+    }
     
-    public void atualizarTudo(){}
-    public void getQtd(){}
-    public void getProduto(){}
+    public String getEndereco() {
+        return endereco;
+    }
 
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTipoFornecedor() {
+        return tipoFornecedor;
+    }
+
+    public void setTipoFornecedor(String tipoFornecedor) {
+        this.tipoFornecedor = tipoFornecedor;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
     public String toString() {
         return "Fornecedor{" + "nome=" + nome + ", endereco=" + endereco + ", telefone=" + telefone + ", tipoFornecedor=" + tipoFornecedor + ", produto=" + produto + ", quantidade=" + quantidade +'}';
     }
