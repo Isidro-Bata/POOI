a595c6357db9ad7fdc5ff4bc3034d89d7ea81dc2
Isidro Bata <bata.isidro@gmail.com>
2021-08-01 21:15:03 +0200

    Verificar

--- src/administracao/Remover.java
+++ src/administracao/Remover.java
@@ -4,11 +4,50 @@
  * and open the template in the editor.
  */
 package administracao;
-
+import entidade.*;
 /**
  *
  * @author vm
  */
 public class Remover {
+    private Fornecedor fornecedor;
+    private Stock estoque;
+    private Produto produto;
+    private Funcionario funcionario;
+    
+    public Remover(){}
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public Stock getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Stock estoque) {
+        this.estoque = estoque;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+    
     
 }
--- src/administracao/Verificar.java
+++ src/administracao/Verificar.java
@@ -3,12 +3,80 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package administracao;
+package Administracao;
+
+import entidade.*;
+import java.util.*;
 
 /**
  *
  * @author vm
  */
 public class Verificar {
+    private Fornecedor fornecedor;
+    private Stock estoque;
+    private Produto produto;
+    private Funcionario funcionario;
+    private Vector vect = new Vector();
+    
+    public Verificar(){}
+    
+    public Object all(String nClass, Vector vect, String info){
+        
+        for(nClass inf : vect){
+            if(inf.getId()== Integer.parseInt(info)){
+                return inf;
+            }
+            if(inf.getNome().equals(info)){
+                return inf;
+            }
+        }
+        return null;
+        
+    }
+    public Fornecedor getFornecedor(String info, Vector<Fornecedor> vect) {
+        /*for(Fornecedor inf : vect){
+            if(inf.getId()== Integer.parseInt(info)){
+                return inf;
+            }
+            if(inf.getNome().equals(info)){
+                return inf;
+            }
+        }*/
+        
+        
+        return (Fornecedor)all("Fornecedor",vect,info);
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public Stock getEstoque(String info, Vector<Stock> vect) {
+       return (Stock)all("Stock",vect,info);
+    }
+
+    public void setEstoque(Stock estoque) {
+        this.estoque = estoque;
+    }
+
+    public Produto getProduto(String info, Vector<Produto> vect) {
+        return (Produto)all("Produto",vect,info);
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Funcionario getFuncionario(String info, Vector<Funcionario> vect) {
+        return (Funcionario)all("Funcionario",vect,info);
+        
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+    
+    
     
 }
